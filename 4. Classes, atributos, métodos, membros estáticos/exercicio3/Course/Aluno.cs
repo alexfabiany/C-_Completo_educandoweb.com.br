994686a909766aaebff992e898e2efbcde462b0b
Alex Fabiany S. Leão <alexfabianyleao@yahoo.com.br>
2020-06-15 17:39:27 -0400

    exercicio #3

---
+++ 4. Classes, atributos, métodos, membros estáticos/exercicio3/Course/Aluno.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Globalization;
+using System.Text;
+
+namespace Course {
+  class Aluno {
+    private string Nome;
+    private double Nota1, Nota2, Nota3;
+    private const double NotaMinima = 70.0;
+
+    public Aluno(string nome, double nota1, double nota2, double nota3) {
+      Nome = nome;
+      Nota1 = nota1;
+      Nota2 = nota2;
+      Nota3 = nota3;
+    }
+
+    public double NotaFinal() {
+      return Nota1 + Nota2 + Nota3;
+    }
+
+    public bool Aprovado() {
+      return NotaFinal() >= NotaMinima;
+    }
+
+    public double NotaRestante() {
+      return Aprovado() ? 0.0 : NotaMinima - NotaFinal();
+    }
+
+    public override string ToString() {
+      string resultado = "NOTA FINAL: " + NotaFinal().ToString("F2", CultureInfo.InvariantCulture);
+
+      if (Aprovado()) {
+       resultado += " << APROVADO >> :)";
+      }
+      else {
+        resultado += " *** REPROVADO *** :("
+          + "\n=> FALTARAM " + NotaRestante().ToString("F2", CultureInfo.InvariantCulture) + " PONTOS!";
+      }
+
+      return resultado;
+    }
+  }
+}
---
+++ 4. Classes, atributos, métodos, membros estáticos/exercicio3/Course/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+
+namespace Course {
+  class Program {
+    static void Main(string[] args) {
+
+      Console.Write("Nome do Aluno: ");
+      string nome = Console.ReadLine();
+      Console.WriteLine("Informa as três notas do aluno: ");
+      double nota1 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+      double nota2 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+      double nota3 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+      Aluno aluno = new Aluno(nome, nota1, nota2, nota3);
+
+      Console.WriteLine(aluno);
+    }
+  }
+}
