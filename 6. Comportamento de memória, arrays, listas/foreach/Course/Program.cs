16fa3ef7373cbe957b6457cd0680ccd8410007ed
Alex Fabiany S. Leão <alexfabianyleao@yahoo.com.br>
2020-06-29 23:04:53 -0400

    Foreach

---
+++ 6. Comportamento de memória, arrays, listas/foreach/Course/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Course {
+  class Program {
+    static void Main(string[] args) {
+
+      string[] nomes = new string[] { "Alex", "Andreas", "Marcíria", "Maria", "Valente" };
+
+      foreach (string nome in nomes) {
+        Console.WriteLine(nome);
+      }
+      
+    }
+  }
+}
