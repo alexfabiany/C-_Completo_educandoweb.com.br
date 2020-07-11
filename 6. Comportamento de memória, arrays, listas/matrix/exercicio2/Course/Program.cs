fc2cc6d5f95079d5e55fa96a4d0ac9a70f0f6cb0
Alex Fabiany <alexfabianyleao@yahoo.com.br>
2020-07-11 16:36:46 -0400

    Matrix - exercicio2

---
+++ 6. Comportamento de memória, arrays, listas/matrix/exercicio2/Course/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Course {
+  class Program {
+    static void Main(string[] args) {
+      Console.Write("Enter the matrix rows and columns count: ");
+      string[] numbers = Console.ReadLine().Split(' ');
+      int m = int.Parse(numbers[0]);
+      int n = int.Parse(numbers[1]);
+
+      int[,] matrix = new int[m, n];
+
+      for (int i = 0; i < m; i++) {
+        string[] values = Console.ReadLine().Split(' ');
+        for (int j = 0; j < n; j++) {
+          matrix[i, j] = int.Parse(values[j]);
+        }
+      }
+
+      Console.Write("Enter a number to find: ");
+      int number = int.Parse(Console.ReadLine());
+
+      for (int i = 0; i < m; i++) {
+        for (int j = 0; j < n; j++) {
+          if (matrix[i, j] == number) {
+            Console.WriteLine("Position " + i + ", " + j + ":");
+            if (j > 0) {
+              Console.WriteLine("Left: " + matrix[i, j - 1]);
+            }
+            if (i > 0) {
+              Console.WriteLine("Up: " + matrix[i - 1, j]);
+            }
+            if (j < n - 1) {
+              Console.WriteLine("Right: " + matrix[i, j + 1]);
+            }
+            if (i < m - 1) {
+              Console.WriteLine("Down: " + matrix[i + 1, j]);
+            }
+          }
+        }
+      }
+    }
+  }
+}
