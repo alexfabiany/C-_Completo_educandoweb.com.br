600cad5d39b9c70065b247b583dea406249377e5
Alex Fabiany <alexfabianyleao@yahoo.com.br>
2020-07-11 17:21:28 -0400

    HashSet - exercicio1

---
+++ 6. Comportamento de memória, arrays, listas/hashset/exercicio1/Course/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Course {
+  class Program {
+    static void Main(string[] args) {
+
+      HashSet<int> A = new HashSet<int>();
+      HashSet<int> B = new HashSet<int>();
+      HashSet<int> C = new HashSet<int>();
+
+      Console.Write("O curso A possui quantos alunos? ");
+      int count = int.Parse(Console.ReadLine());
+
+      Console.WriteLine("Digite os códigos dos alunos do curso A:");
+      for (int i = 0; i < count; i++) {
+        int codigo = int.Parse(Console.ReadLine());
+        A.Add(codigo);
+      }
+
+      Console.Write("O curso B possui quantos alunos? ");
+      count = int.Parse(Console.ReadLine());
+
+      Console.WriteLine("Digite os códigos dos alunos do curso B:");
+      for (int i = 0; i < count; i++) {
+        int codigo = int.Parse(Console.ReadLine());
+        B.Add(codigo);
+      }
+
+      Console.Write("O curso C possui quantos alunos? ");
+      count = int.Parse(Console.ReadLine());
+
+      Console.WriteLine("Digite os códigos dos alunos do curso C:");
+      for (int i = 0; i < count; i++) {
+        int codigo = int.Parse(Console.ReadLine());
+        C.Add(codigo);
+      }
+
+      HashSet<int> total = new HashSet<int>();
+      total.UnionWith(A);
+      total.UnionWith(B);
+      total.UnionWith(C);
+
+      Console.WriteLine("Total de alunos: " + total.Count);
+      Console.ReadLine();
+    }
+  }
+}
