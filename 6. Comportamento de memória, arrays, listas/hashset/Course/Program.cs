ea4578955a3d684032d946e4dd560a971d735d34
Alex Fabiany <alexfabianyleao@yahoo.com.br>
2020-07-11 17:05:07 -0400

    HashSet

---
+++ 6. Comportamento de memória, arrays, listas/hashset/Course/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Course {
+  class Program {
+    static void Main(string[] args) {
+
+      HashSet<int> A = new HashSet<int>();
+      HashSet<int> B = new HashSet<int>();
+      HashSet<int> C = new HashSet<int>();
+
+      A.Add(3);
+      A.Add(5);
+      A.Add(8);
+      A.Add(9);
+
+      B.Add(3);
+      B.Add(4);
+      B.Add(5);
+
+       A.UnionWith(B);
+      //A.IntersectWith(B);
+      // A.ExceptWith(B);
+
+      foreach (int i in A) {
+        Console.WriteLine(i);
+      }
+    }
+  }
+}
