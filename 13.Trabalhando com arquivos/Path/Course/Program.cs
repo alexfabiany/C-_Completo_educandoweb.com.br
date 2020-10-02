2078cdb5aede139fb4c0d891388c9b0c5385f8da
Alex Fabiany <alexfabianyleao@yahoo.com.br>
2020-10-02 16:55:32 -0400

    Trabalhando com arquivos [Path]

---
+++ 13.Trabalhando com arquivos/Path/Course/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace Course {
+    class Program {
+        static void Main(string[] args) {
+            string path = @"c:\lixo\myfolder\file1.txt";
+
+            try {
+                Console.WriteLine("DirectorySeparatorChar: " + Path.DirectorySeparatorChar);
+                Console.WriteLine("PathSeparator: " + Path.PathSeparator);
+                Console.WriteLine("GetDirectoryName: " + Path.GetDirectoryName(path));
+                Console.WriteLine("GetFileName: " + Path.GetFileName(path));
+                Console.WriteLine("GetFileNameWithoutExtension: " + Path.GetFileNameWithoutExtension(path));
+                Console.WriteLine("GetExtension: " + Path.GetExtension(path));
+                Console.WriteLine("GetFullPath: " + Path.GetFullPath(path));
+                Console.WriteLine("GetTempPath: " + Path.GetTempPath());
+            } catch (IOException e) {
+                Console.WriteLine("An error occurred!");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
