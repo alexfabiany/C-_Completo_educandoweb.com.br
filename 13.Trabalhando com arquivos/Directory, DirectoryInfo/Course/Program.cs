d54da9dfead46a50f8b219da0876e1b99da55a65
Alex Fabiany <alexfabianyleao@yahoo.com.br>
2020-10-02 16:40:56 -0400

    Trabalhando com arquivos [Directory, DirectoryInfo]

---
+++ 13.Trabalhando com arquivos/Directory, DirectoryInfo/Course/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace Course {
+    class Program {
+        static void Main(string[] args) {
+            string path = @"c:\lixo\myfolder";
+
+            try {
+                //IEnumerable<string> folders = Directory.EnumerateDirectories(path, "*.*", SearchOption.AllDirectories);
+                var folders = Directory.EnumerateDirectories(path, "*.*", SearchOption.AllDirectories);
+                foreach (string s in folders) {
+                    Console.WriteLine("FOLDER:");
+                    Console.WriteLine(s);
+
+                    var files = Directory.EnumerateFiles(s, "*.*", SearchOption.AllDirectories);
+                    //Console.WriteLine("FILES:");
+                    foreach (string f in files) {
+                        Console.WriteLine(f);
+                    }
+                }
+                //Directory.CreateDirectory(path + @"\newfolder");
+            } catch (IOException e) {
+                Console.WriteLine("An error occurred!");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
