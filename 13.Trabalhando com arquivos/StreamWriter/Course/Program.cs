0e65706c3223ac16e13196c028a28c3b35531d17
Alex Fabiany <alexfabianyleao@yahoo.com.br>
2020-10-02 16:19:26 -0400

    Trabalhando com arquivos [StreamWriter]

---
+++ 13.Trabalhando com arquivos/StreamWriter/Course/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+
+namespace Course {
+    class Program {
+        static void Main(string[] args) {
+            string sourcePath = @"c:\lixo\file1.txt";
+            string targetPath = @"c:\lixo\file2.txt";
+
+            try {
+                string[] lines = File.ReadAllLines(sourcePath);
+
+                using(StreamWriter sw = File.AppendText(targetPath)) {
+                    foreach (string line in lines) {
+                        sw.WriteLine(line.ToUpper());
+                    }
+                }
+            } catch (IOException e) {
+                Console.WriteLine("An error occurred!");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
