8a943f62e1b20b2646e631a256b478438aa39c14
Alex Fabiany <alexfabianyleao@yahoo.com.br>
2020-11-15 19:18:10 -0400

    Heranca e Polimorfismo [Heranca (modificador protected)]

---
+++ 10.Herança e Polimorfismo/heranca/Course/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Course.Entities;
+
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BusinessAccount account = new BusinessAccount(1010, "Alex Fabiany", 10000.0, 5000.0);
+            Console.WriteLine(account.Balance);
+            // account.Balance = 20000.0;
+        }
+    }
+}
