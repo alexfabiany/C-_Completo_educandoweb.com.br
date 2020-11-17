c62ab46f70b6a2f0bf2824122546c40cc7850077
Alex Fabiany <alexfabianyleao@yahoo.com.br>
2020-11-17 19:32:13 -0400

    Heranca e Polimorfismo [exercicio 1]

---
+++ 10.Herança e Polimorfismo/exercicio01/Course/Entities/OutsourcedEmployee.cs
@@ -0,0 +1,27 @@
+﻿using System.Globalization;
+
+namespace Course.Entities
+{
+    class OutsourcedEmployee : Employee
+    {
+        public double AdditionalCharge { get; private set; }
+
+        public OutsourcedEmployee() { }
+
+        public OutsourcedEmployee(string name, int hours, double valuePerHour, double additionalCharge)
+            : base(name, hours, valuePerHour)
+        {
+            AdditionalCharge = additionalCharge;
+        }
+
+        public override double Payment()
+        {
+            return base.Payment() + (AdditionalCharge * 1.1);
+        }
+
+        //public override string ToString()
+        //{
+        //    return $"{Name} - $ {this.Payment().ToString("F2", CultureInfo.InvariantCulture)}";
+        //}
+    }
+}
