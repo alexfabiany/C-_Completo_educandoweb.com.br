3ffb3b5f0174e7a7ce628419da1a9951e36342fe
Alex Fabiany <alexfabianyleao@yahoo.com.br>
2020-11-17 17:21:34 -0400

    Heranca e Polimorfismo [virtual/override/base]

---
+++ 10.Herança e Polimorfismo/sobreposicao/Course/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Course.Entities;
+
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Account account1 = new Account(1001, "Alex Fabiany", 500.0);
+            Account account2 = new SavingsAccount(1002, "Marcíria Lima", 500.0, 0.01);
+
+            account1.Withdraw(10.0);
+            account2.Withdraw(10.0);
+
+            Console.WriteLine(account1.Balance);
+            Console.WriteLine(account2.Balance);
+        }
+    }
+}
