8b228f607eb9c5693d384eb3381a3ab0b8078d23
Alex Leão <alex@patrimonio.com>
2020-12-28 13:50:42 -0400

    Projeto jogo de xadrez

---
+++ xadrez-console/xadrez-console/tabuleiro/Peca.cs
@@ -0,0 +1,17 @@
+using tabuleiro;
+
+namespace tabuleiro {
+    class Peca {
+        public Posicao posicao { get; set; }
+        public Cor cor { get; set; }
+        public int qtdeMovimentos { get; protected set; }
+        public Tabuleiro tab { get; protected set; }
+
+        public Peca( Tabuleiro tab, Cor cor ) {
+            this.posicao = null;
+            this.tab = tab;
+            this.cor = cor;
+            this.qtdeMovimentos = 0;
+        }
+    }
+}
